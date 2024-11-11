90042a12e976ba70d8d21cd46073504dca235447
Berk Yalcinkaya <berk@Berks-MacBook-Pro.local>
2024-11-10 19:04:02 -0800

    ADD: processing data

--- embpred/data/make_dataset.py
+++ embpred/data/make_dataset.py
@@ -325,7 +325,7 @@ if __name__ == "__main__":
     # for dataset, mapping in zip(datasets, mappings):
     #     process_by_focal_depth(dataset, f'carson-224-3depths', mapping, use_GPU=True, depths=["F-15", "F0", "F15"], 
     #                            target_size=(224, 224), pad_images=False, output_ext="jpeg")
-    paths, labels = get_all_image_paths_from_raws(loc = INTERIM_DATA_DIR / "'carson-224-3depths", im_type="jpeg")
+    paths, labels = get_all_image_paths_from_raws(loc = INTERIM_DATA_DIR / "carson-224-3depths", im_type="jpeg")
     with open(RAW_DATA_DIR / "mappings.json", "r") as f:
         mappings = json.load(f)
     make_dataset(mappings, paths, labels, dataset_additional_text="carson-224-3depths")
