a234ad410918e4044dc0054d1c77613933c75713
Berk Yalcinkaya <berk@Berks-MacBook-Pro.local>
2024-11-10 16:01:54 -0800

    ADD: reprocess data

--- embpred/data/make_dataset.py
+++ embpred/data/make_dataset.py
@@ -240,10 +240,10 @@ def process_embryo(emb_dir, depths, label_json, label_key, model, device, output
                 image = focus_and_pad(image, target_size, model, device)
             else:
                 image = extract_emb_frame_2d(image, model, device)
-                image = resize(image, target_size, anti_aliasing=True)
+                image = resize(image, target_size, anti_aliasing=True, preserve_range=True)
             ims.append(image)
-        ims = np.stack(ims, axis=0)
-        assert ims.shape == (len(depths), target_size[0], target_size[1])
+        ims = np.stack(ims, axis=-1)
+        assert ims.shape == ( target_size[0], target_size[1], len(depths))
         imsave(im_file, ims)
 
 def process_by_focal_depth(directory, output_dir, label_json, use_GPU=True, classes_to_use=None, 
