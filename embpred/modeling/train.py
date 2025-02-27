c018f55578fe99dd45338e94d6c907bc035eee2b
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2025-02-27 11:49:35 -0800

    ADD: BigWNet

--- embpred/modeling/train.py
+++ embpred/modeling/train.py
@@ -65,15 +65,15 @@ def do_random_sample(PRE_RANDOM_SAMPLE, files, labels):
 if __name__ == "__main__":
     # Define the models to train with 
     MODELS = [
-        ("Wnet-224-emb-kfolds", WNet, {"dropout":False, "dropout_rate":0.5}),
-        ("BigWnet-224-emb-kfolds", WNet, {"dropout":False}),
-        ("BigWnet-drop-224-emb-kfolds", WNet, {"dropout":True, "dropout_rate":0.5})
+        ("Wnet-embSplits", WNet, {"dropout":False, "dropout_rate":0.5}),
+        ("BigWnet-embSplits", WNet, {"dropout":False}),
+        ("BigWnet-drop-embSplits", WNet, {"dropout":True, "dropout_rate":0.5})
         #("BiggerNet3D224-emb-kfolds-noUpSample", BiggerNet3D224, {})
         #("CustomResNet18-1layer-full-balance", CustomResNet18, {"num_dense_layers": 1, "dense_neurons": 64, "input_shape": (3, 224, 224)}),
     ]
 
     KFOLDS = 1
-    EPOCHS = 100
+    EPOCHS = 50
     LR = 0.001 
     WEIGHT_DECAY = 0.0001
     BATCH_SIZE = 64
