7a8106dfef87638ce17336cd42b9758648fad35d
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2025-03-11 23:46:11 -0700

    add script to get example images

---
+++ embpred/data/get_test_images.py
@@ -0,0 +1,34 @@
+from embpred.config import RAW_DATA_DIR, PROJ_ROOT
+import json
+import os
+from glob import glob
+import numpy as np
+from skimage.io import imread, imsave
+
+output_dir = PROJ_ROOT / "reports" / "sample_images"
+embryo = "Subj_26790_Emb1"
+mapping_file = RAW_DATA_DIR / "output.json"
+image_dir = RAW_DATA_DIR / "DatasetNew" / embryo / "F0"
+images = sorted(glob(os.path.join(image_dir, "*.jpeg")), key = lambda x: int(x.split(".")[-2].split("RUN")[-1]))
+
+
+with open(mapping_file) as f:
+    mapping = json.load(f)
+embryo_map = mapping[embryo]
+
+labels = embryo_map.keys()
+unique_labels = np.unique(list(labels))
+
+for i, image_path in  enumerate(images):
+    image_name = os.path.basename(image_path)
+    label = embryo_map[image_name]
+    if label in unique_labels:
+        unique_labels = np.delete(unique_labels, np.where(unique_labels == label))
+        print(f"Image {i} has label {label}")
+        print(f"Unique labels left: {unique_labels}")
+        image = imread(image_path)
+        imsave(str(output_dir / label) + ".jpeg", image)
+    if len(unique_labels) == 0:
+        break
+
+
