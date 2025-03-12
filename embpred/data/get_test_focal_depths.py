aabbff6ea95c1f3b4d9660087ea655a99e67e99e
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2025-03-12 16:25:35 -0700

    ADD: logging on focal depth extraction script

--- embpred/data/get_test_focal_depths.py
+++ embpred/data/get_test_focal_depths.py
@@ -6,7 +6,9 @@ import numpy as np
 from skimage.io import imread, imsave
 from embpred.features import load_faster_RCNN_model_device, ExtractEmbFrame, extract_emb_frame_2d
 from skimage.transform import resize
+from loguru import logger
 
+logger.info("Running focal depth extraction script")
 output_dir = PROJ_ROOT / "reports" / "sample_focal_depths"
 
 if not os.path.exists(output_dir):
@@ -19,13 +21,15 @@ image_dir = RAW_DATA_DIR / "DatasetNew" / embryo
 depths = ["F-45", "F-30", "F-15","F0", "F15", "F30", "F45"]
 depth_ims = []
 for depth in depths:
+    logger.info(f"Extracting focal depth {depth}")
     depth_dir = image_dir / depth
     images = sorted(glob(os.path.join(depth_dir, "*.jpeg")), key = lambda x: int(x.split(".")[-2].split("RUN")[-1]))
     depth_ims.append(images[10])
-    print(len(images))
+    logger.info(f"Found {len(images)} images for focal depth {depth}")
 
 for depth_im, depth in zip(depth_ims, depths):
     imsave(str(output_dir / depth) + ".jpeg", imread(depth_im))
+    logger.info(f"Saved image for focal depth {depth}")
 
 # merge "F-15","F0", "F15" into an RGB image and save
 imNeg15 = imread(depth_ims[2])
@@ -33,9 +37,10 @@ im0 = imread(depth_ims[3])
 im15 = imread(depth_ims[4])
 
 bbox_ims = []
+logger.info("Extracting bounding boxes")
 for im in [im15, im0, imNeg15]:
     bbox_im = extract_emb_frame_2d(im)
-    print(bbox_im.shape)
+    logger.info(f"Extracted bounding box of shape {bbox_im.shape}")
     bbox_im = resize(bbox_im, (224,224), anti_aliasing=True, preserve_range=True)
     bbox_ims.append(bbox_im)
 
@@ -44,4 +49,5 @@ for im in [im15, im0, imNeg15]:
 merged_im = np.stack(bbox_ims, axis=2)
 assert(merged_im.shape == (224,224,3))
 imsave(str(output_dir / "merged_cropped_resized") + ".jpeg", merged_im) 
+logger.info(f"Saved merged image")
 
